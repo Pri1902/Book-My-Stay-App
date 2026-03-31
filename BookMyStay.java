2bb2d1a71999bb7b512507dfaee89273611d0c54
Pri1902 <priyankapraveenajain1902@gmail.com>
2026-04-01 03:43:47 +0530

    UC1: Welcome Message

--- BookMyStay.java
+++ BookMyStay.java
@@ -1,5 +1,7 @@
 public class BookMyStay{
     public static void main(String[] args) {
+        System.out.println("Welcome to Hotel Booking Management System");
+        System.out.println("System Initialized successfully...");
         
     }
 }
