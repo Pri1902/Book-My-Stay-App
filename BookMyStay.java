1c5bfc39a34fba09396e75430ecfbb936e3dd64c
Pri1902 <priyankapraveenajain1902@gmail.com>
2026-04-01 04:06:10 +0530

    UC3: Room Types(Hash Maps)

--- BookMyStay.java
+++ BookMyStay.java
@@ -1,3 +1,6 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class BookMyStay {
 
     // Abstract Room class
@@ -19,12 +22,12 @@ public class BookMyStay {
             System.out.println("Beds: " + beds);
             System.out.println("Size: " + size + " sqft");
             System.out.println("Price per night: " + pricePerNight);
-            System.out.println("Available: " + availability);
+            System.out.println("Available Rooms: " + availability);
             System.out.println();
         }
     }
 
-    // ✅ Make subclasses static
+    // Concrete Room Classes
     static class SingleRoom extends Room {
         public SingleRoom() {
             super(1, 250, 1500.0);
@@ -55,19 +58,64 @@ public class BookMyStay {
         }
     }
 
+    // Inventory class using HashMap
+    static class Inventory {
+        private Map<String, Integer> availabilityMap;
+
+        // Constructor initializes availability
+        public Inventory() {
+            availabilityMap = new HashMap<>();
+            availabilityMap.put("Single", 5);
+            availabilityMap.put("Double", 3);
+            availabilityMap.put("Suite", 2);
+        }
+
+        // Get availability
+        public int getAvailability(String roomType) {
+            return availabilityMap.getOrDefault(roomType, 0);
+        }
+
+        // Add rooms (increase availability)
+        public void addRooms(String roomType, int count) {
+            availabilityMap.put(roomType,
+                    getAvailability(roomType) + count);
+        }
+
+        // Book room (decrease availability safely)
+        public boolean bookRoom(String roomType) {
+            int available = getAvailability(roomType);
+            if (available > 0) {
+                availabilityMap.put(roomType, available - 1);
+                return true;
+            } else {
+                System.out.println("No " + roomType + " rooms available!");
+                return false;
+            }
+        }
+
+        // Display full inventory
+        public void displayInventory(Room room) {
+            int available = getAvailability(room.getRoomType());
+            room.displayDetails(available);
+        }
+    }
+
     public static void main(String[] args) {
+
+        // Initialize rooms
         Room single = new SingleRoom();
         Room doubleRoom = new DoubleRoom();
         Room suite = new SuiteRoom();
 
-        int singleAvailable = 5;
-        int doubleAvailable = 3;
-        int suiteAvailable = 2;
+        // Initialize inventory
+        Inventory inventory = new Inventory();
 
-        System.out.println("Hotel Room Initialization:\n");
+        System.out.println("Hotel Room Inventory Status\n");
 
-        single.displayDetails(singleAvailable);
-        doubleRoom.displayDetails(doubleAvailable);
-        suite.displayDetails(suiteAvailable);
+        // Display initial inventory
+        inventory.displayInventory(single);
+        inventory.displayInventory(doubleRoom);
+        inventory.displayInventory(suite);
+
     }
 }
